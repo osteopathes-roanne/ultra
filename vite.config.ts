87397a8e2aacde518540ad421a1d638a0af6d78d
quentin <quentin.goujon@epita.fr>
2025-03-28 18:51:15 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/osteo-site/', // ✅ important pour GitHub Pages
+  base: '/ultra/', // ✅ important pour GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
